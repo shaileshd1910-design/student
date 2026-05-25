351bf9377f7539797cc3278010ce11eaaa461900
Shailesh1-D <shaileshd1910@gmail.com>
2026-05-25 20:15:49 +0530

    full project

--- backend/server.js
+++ backend/server.js
@@ -103,7 +103,7 @@ app.post("/login", async (req, res) => {
 
 app.get("/", (req, res) => {
 
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.sendFile(path.join(__dirname, "frontend", "admin.html"));
 
 });
 
